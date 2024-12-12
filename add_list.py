54f9b636a855aa1b29558197146a02cfda9d0267
Влад <dubasovec@gmail.com>
2024-12-12 21:29:31 +0300

    Первый этап (5 заданий)

--- add_list.py
+++ add_list.py
@@ -1,15 +1,23 @@
-#GRADE 1. ЭТАП 1. Задание 2.
+#GRADE 1. ЭТАП 1. Задание 4.
 #ВВОД ПОЛЬЗОВАТЕЛЬСКИХ ДАННЫХ
 username = input('Введите имя: ')
-title = input('Введите название: ')
 content = input('Введите описание: ')
 status = input('Введите статус (Активна / Завершена): ')
 created_date = input('Ввведите дату создания (день-месяц-год): ')
 issue_date = input('Ввведите дату завершения (день-месяц-год): ')
 
+#ОБЪЯВЛЕНИЕ СПИСКА
+titles_lst = []
+
+#ВВОД ЗАГОЛОВКОВ ПО СРЕДСТВАМ ЦИКЛА
+for item in range(3):
+    title = input(f'Введите заголовок заметки {item + 1}: ')
+    titles_lst.append(title)
+
 #ВЫВОД ПОЛЬЗОВАТЕЛЬСКИХ ДАННЫХ
-print('\nИмя пользователя: ', username)
-print('Название: ', title)
+print('\nВведены следующие данные:')
+print('Имя пользователя: ', username)
+print('Название: ', titles_lst)
 print('Описание: ', content)
 print('Статус: ', status)
 print('Дата создания: ', created_date[0:5])
